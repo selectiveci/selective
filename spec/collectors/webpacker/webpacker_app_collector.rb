c7d4a8ac028f90aaf9a5e1f8690da689dfeba18d
nshoes <nate@hint.io>
2020-08-05 09:34:50 -0700

    dont use instance variable

--- spec/collectors/webpacker/webpacker_app_collector.rb
+++ spec/collectors/webpacker/webpacker_app_collector.rb
@@ -8,11 +8,10 @@ RSpec.describe Selective::Collectors::Webpacker::WebpackerAppCollector do
     let(:dummy_js_dir) { 'spec/dummy/app/javascript' }
 
     before do
-      @mock_collector = double
       allow(Selective).to receive(:coverage_collectors).and_return({
-        Selective::Collectors::Webpacker::WebpackerAppCollector => @mock_collector
+        Selective::Collectors::Webpacker::WebpackerAppCollector => mock_collector
       })
-      allow(@mock_collector).to receive(:on_start)
+      allow(mock_collector).to receive(:on_start)
 
       Selective.start_coverage
       Selective.configure do |config|
@@ -22,9 +21,10 @@ RSpec.describe Selective::Collectors::Webpacker::WebpackerAppCollector do
 
     context 'when a view is extended with Webpacker::Helper'do
       let(:view) { DummyView.new(::ActionView::LookupContext.new([]), {}) }
+      let(:mock_collector) { double }
 
       it 'is not called' do
-        expect(@mock_collector).not_to receive(:add_covered_globs)
+        expect(mock_collector).not_to receive(:add_covered_globs)
         view.extend(::Webpacker::Helper)
         view.render(inline: '<% javascript_packs_with_chunks_tag "foo" %>')
       end
@@ -32,9 +32,10 @@ RSpec.describe Selective::Collectors::Webpacker::WebpackerAppCollector do
 
     context 'when a view is extended with Selective::Collectors::Webpacker::Helpers' do
       let(:view) { DummyView.new(::ActionView::LookupContext.new([]), {}) }
+      let(:mock_collector) { double }
 
       it 'is called' do
-        expect(@mock_collector).to receive(:add_covered_globs).with("#{dummy_js_dir}/foo/src/**.{scss,css,js}", "#{dummy_js_dir}/foo/package*.json")
+        expect(mock_collector).to receive(:add_covered_globs).with("#{dummy_js_dir}/foo/src/**.{scss,css,js}", "#{dummy_js_dir}/foo/package*.json")
         view.extend(Selective::Collectors::Webpacker::Helpers)
         view.render(inline: '<% javascript_packs_with_chunks_tag "foo" %>')
       end
