b02f8c264439aa0ded96ddc0dd4c8b73cd42ea8a
nshoes <nate@hint.io>
2020-08-06 13:58:36 -0700

    specs for AttributeReaderCollector, start re-organizing spec setup

--- spec/collectors/active_record/attribute_reader_helper_spec.rb
+++ spec/collectors/active_record/attribute_reader_helper_spec.rb
@@ -1,40 +1,44 @@
 require "spec_helper"
 
 RSpec.describe Selective::Collectors::ActiveRecord::AttributeReaderHelper do
-  let(:mock_collector) { double }
+  let(:collector) { Selective.coverage_collectors[Selective::Collectors::ActiveRecord::AttributeReaderCollector] }
 
-  before do
-    allow(Selective).to receive(:coverage_collectors).and_return({
-      described_class.parent::AttributeReaderCollector => mock_collector
-    })
-
-    Selective.start_coverage
+  before(:each, :full_setup) do
+    allow(Selective).to receive(:enabled?).and_return true
+    allow(Selective).to receive(:initialize_rspec_hooks)
+    Selective.initialize_collectors
   end
 
   describe "#add_covered_models" do
     context 'when selective is not enabled' do
+      let(:collector) { double }
+
+      before do
+        allow(Selective).to receive(:coverage_collectors).and_return({
+          described_class.parent::AttributeReaderCollector => collector
+        })
+      end
+
       it 'is not called' do
-        expect(mock_collector).not_to receive(:add_covered_models)
+        expect(collector).not_to receive(:add_covered_models)
 
         a_dummy = ADummy.new
         a_dummy.attr1
       end
     end
 
-    context 'when selective is enabled' do
+    context 'when selective is enabled', :full_setup do
       it 'is called' do
-        expect(mock_collector).to receive(:add_covered_models).with(ADummy)
+        expect(collector).to receive(:add_covered_models).with(ADummy)
 
         a_dummy = ADummy.new
-        a_dummy.extend(described_class)
         a_dummy.attr1
       end
 
       it 'is not called on attributes that don\'t exist' do
-        expect(mock_collector).not_to receive(:add_covered_models)
+        expect(collector).not_to receive(:add_covered_models)
 
         a_dummy = ADummy.new
-        a_dummy.extend(described_class)
         expect { a_dummy.attr2 }.to raise_error(NoMethodError)
       end
     end
--- spec/collectors/webpacker/webpacker_app_collector_spec.rb
+++ spec/collectors/webpacker/webpacker_app_collector_spec.rb
@@ -7,35 +7,41 @@ RSpec.describe Selective::Collectors::Webpacker::WebpackerAppCollector do
   describe "#add_covered_globs" do
     let(:dummy_js_dir) { 'spec/dummy/app/javascript' }
 
-    before do
-      allow(Selective).to receive(:coverage_collectors).and_return({
-        Selective::Collectors::Webpacker::WebpackerAppCollector => mock_collector
-      })
-      allow(mock_collector).to receive(:on_start)
-
-      Selective.start_coverage
-      Selective.configure do |config|
-        config.webpacker_app_locations = [dummy_js_dir]
-      end
-    end
-
-    context 'when selective is not enabled'do
+    context 'when selective is disabled' do
       let(:view) { DummyView.new(::ActionView::LookupContext.new([]), {}) }
-      let(:mock_collector) { double }
+      let(:collector) { double }
+
+      before do
+        allow(Selective).to receive(:coverage_collectors).and_return({
+          described_class => collector
+        })
+      end
 
       it 'is not called' do
-        expect(mock_collector).not_to receive(:add_covered_globs)
+        expect(collector).not_to receive(:add_covered_globs)
         view.extend(::Webpacker::Helper)
         view.render(inline: '<% javascript_packs_with_chunks_tag "foo" %>')
       end
     end
 
-    context 'when selective is enabled' do
+    context 'when selective is enabled', :full_setup do
       let(:view) { DummyView.new(::ActionView::LookupContext.new([]), {}) }
-      let(:mock_collector) { double }
+      let(:collector) { double }
+      
+      before do
+        allow(Selective).to receive(:enabled?).and_return true
+        allow(Selective).to receive(:initialize_rspec_hooks)
+  
+        Selective.initialize_collectors
+        Selective.configure do |config|
+          config.webpacker_app_locations = [dummy_js_dir]
+        end
+  
+        Selective.coverage_collectors[described_class] = collector
+      end
 
       it 'is called' do
-        expect(mock_collector).to receive(:add_covered_globs).with("#{dummy_js_dir}/foo/src/**.{scss,css,js}", "#{dummy_js_dir}/foo/package*.json")
+        expect(collector).to receive(:add_covered_globs).with("#{dummy_js_dir}/foo/src/**.{scss,css,js}", "#{dummy_js_dir}/foo/package*.json")
         view.extend(Selective::Collectors::Webpacker::Helpers)
         view.render(inline: '<% javascript_packs_with_chunks_tag "foo" %>')
       end
