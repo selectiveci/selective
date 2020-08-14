9e26b4af07a5484583aef3554ae1ab6fb3c54e66
Jason Dinsmore <jason@dinjas.com>
2020-08-14 06:46:04 -0700

    Continue expanding spec for Selective module

--- spec/lib/selective_spec.rb
+++ spec/lib/selective_spec.rb
@@ -31,8 +31,98 @@ RSpec.describe Selective do
   end
 
   describe ".configure" do
-    xit "sets config ivar" do
+    before do
+     # described_class.instance_variable_set(:@config, nil)
+    end
 
+    after do
+      described_class.instance_variable_set(:@config, nil)
+    end
+
+    it "sets config ivar" do
+      result = described_class.configure {}
+
+      expect(result).to be_nil
+      expect(described_class.instance_variable_get(:@config))
+        .to be_an_instance_of(Selective::Config)
+    end
+
+    it "yields" do
+      result = described_class.configure { |_| :foo }
+
+      expect(result).to equal(:foo)
+      expect(described_class.instance_variable_get(:@config))
+        .to be_an_instance_of(Selective::Config)
+    end
+  end
+
+  describe ".config" do
+    before do
+      #described_class.instance_variable_set(:@config, nil)
+    end
+
+    after do
+      described_class.instance_variable_set(:@config, nil)
+    end
+
+    it "sets config ivar" do
+      result = described_class.config
+
+      expect(result).to be_an_instance_of(Selective::Config)
+      expect(described_class.instance_variable_get(:@config))
+        .to eql(result)
+    end
+  end
+
+  describe ".initialize_collectors" do
+    context "when enabled" do
+      before do
+        allow(described_class).to receive(:enabled?).and_return(true)
+        described_class.config
+      end
+
+      after do
+        described_class.instance_variable_set(:@config, nil)
+        described_class.instance_variable_set(:@collector, nil)
+      end
+
+      it "sets collector ivar" do
+        allow(described_class).to receive(:initialize_rspec_hooks)
+
+        described_class.initialize_collectors
+
+        expect(described_class.collector)
+          .to be_an_instance_of(Selective::Collector)
+      end
+
+      it "initializes rspec hooks" do
+        expect(described_class).to receive(:initialize_rspec_hooks)
+
+        described_class.initialize_collectors
+      end
+    end
+
+    context "when not enabled" do
+      before do
+        allow(described_class).to receive(:enabled?).and_return(false)
+        described_class.config
+      end
+
+      after do
+        described_class.instance_variable_set(:@config, nil)
+      end
+
+      it "does not set collector ivar" do
+        described_class.initialize_collectors
+
+        expect(described_class.collector).to be_nil
+      end
+
+      it "does not initialize rspec hooks" do
+        expect(described_class).not_to receive(:initialize_rspec_hooks)
+
+        described_class.initialize_collectors
+      end
     end
   end
 end
