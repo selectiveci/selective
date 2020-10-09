653929d18f48f2b2f38828161b55cab36dddcfe6
Joel Hayhurst <joelmichael@gmail.com>
2020-10-09 13:40:51 -0700

    Minor test_selection.rb cleanup
    - Add a comment describing the override
    - Use single quotes instead of double quotes where appropriate
    - Use long form "--name" instead of "-n"

--- lib/selective/minitest/test_selection.rb
+++ lib/selective/minitest/test_selection.rb
@@ -1,9 +1,11 @@
 module Selective::MinitestSelectionPlugin
+  # Alter process_args to insert a name filter into the arguments list
+  # This will override any other name filters that are passed
   def process_args(args = [])
     Selective.selected_tests = Selective::Selector.tests_from_diff
     if Selective.selected_tests.any?
-      regex = '/' + Selective.selected_tests.join("|") + '/'
-      args.concat(['-n', regex])
+      regex = '/' + Selective.selected_tests.join('|') + '/'
+      args.concat(['--name', regex])
     end
     super
   end
