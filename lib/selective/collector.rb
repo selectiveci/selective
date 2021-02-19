331b78cb2add3ecefbbacc3eee42519e77e78466
Jason Dinsmore <jason@dinjas.com>
2021-02-19 13:40:23 -0800

    Remove debug; tidy up

--- lib/selective/collector.rb
+++ lib/selective/collector.rb
@@ -58,13 +58,12 @@ module Selective
     end
 
     def payloads
-      data = Selective::Storage.load(config.coverage_path)
+      coverage_data = Selective::Storage.load(config.coverage_path)
       root = "#{Rails.root}/"
 
-      puts "Data size: #{data.size}"
-      data.each_slice(1000).map do |slice|
-        call_graph_data = Hash[slice.map { |k, v| [k, v.keys.map { |f| f.sub(root, "") }] }]
-        call_graph_hash(call_graph_data)
+      coverage_data.each_slice(1000).map do |slice|
+        data = slice.map { |k, v| [k, v.keys.map { |f| f.sub(root, "") }] }
+        call_graph_hash(Hash[data])
       end
     end
 
